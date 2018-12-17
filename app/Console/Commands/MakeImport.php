470dcbc4ed437a2d056f0919e2359dcde96a5cce
Seliv <selivonenko007@gmail.com>
2018-12-17 22:12:46 +0200

    console start import

---
+++ app/Console/Commands/MakeImport.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\TecDoc\ImportPriceList;
+use Illuminate\Console\Command;
+
+class MakeImport extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'import:start';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Start import price-list';
+
+    /**
+     * Create a new command instance.
+     *
+     * @param ImportPriceList $importPriceList
+     */
+    public function __construct()
+    {
+        parent::__construct();
+
+        new ImportPriceList();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        //
+    }
+}
