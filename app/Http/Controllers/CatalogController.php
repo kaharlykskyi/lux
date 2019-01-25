758876dc4790a3fbf43272f4d97b1de370b02342
Seliv <selivonenko007@gmail.com>
2019-01-25 12:23:00 +0200

    filter update

--- app/Http/Controllers/CatalogController.php
+++ app/Http/Controllers/CatalogController.php
@@ -12,11 +12,13 @@ class CatalogController extends Controller
 
     protected $pre_products = 12;
 
-    protected $filter = null;
-
-    protected $filter_whereIN = null;
-
     protected $brands = [];
+
+    protected $attribute = [];
+
+    protected $in_stock = [];
+
+    protected $not_stock = [];
 
     public function __construct()
     {
@@ -28,7 +30,6 @@ class CatalogController extends Controller
         $products = [];
         $min_price = 0;
         $max_price = 0;
-        $attribute = [];
 
         if(session('pre_products')){
             $this->pre_products = session('pre_products');
@@ -99,30 +100,15 @@ class CatalogController extends Controller
             return redirect()->route('home');
         }
 
-
-        $in_stock = [];
-        $not_stock = [];
         foreach ($products as $product){
-            $attr = $this->tecdoc->getArtAttributes($product->DataSupplierArticleNumber,$product->supplierId);
-            foreach ($attr as $item){
-                $attribute[$item->id][] = $item;
-            }
-            $flag_add_brand = true;
             $catalog_product = Product::where('articles',str_replace(' ','',$product->DataSupplierArticleNumber))->first();
-            if (isset($catalog_product)){
-                $product->price = $catalog_product->price;
-                if ($product->price < $min_price || $min_price === 0){
-                    $min_price = $product->price;
-                }
-                if ($product->price > $max_price || $max_price === 0){
-                    $max_price = $product->price;
-                }
-                $product->id = $catalog_product->id;
-                $product->name = $catalog_product->name;
-                $in_stock[] = $product;
-            }else{
-                $not_stock[] = $product;
+            $flag_add_brand = true;
+            $attr = $this->tecdoc->getArtAttributes($product->DataSupplierArticleNumber,$product->supplierId);
+            $product->filter_data = $attr;
+            foreach ($attr as $item){
+                $this->attribute[$item->id][] = $item;
             }
+
             foreach ($this->brands as $brand){
                 if ($brand->supplierId === $product->supplierId){
                     $flag_add_brand = false;
@@ -134,9 +120,26 @@ class CatalogController extends Controller
                     'description' => $product->matchcode
                 ];
             }
+
+            if (isset($catalog_product)){
+                $product->price = $catalog_product->price;
+                if ($product->price < $min_price || $min_price === 0){
+                    $min_price = $product->price;
+                }
+                if ($product->price > $max_price || $max_price === 0){
+                    $max_price = $product->price;
+                }
+                $product->id = $catalog_product->id;
+                $product->name = $catalog_product->name;
+                $this->in_stock[] = $product;
+            }else{
+                $this->not_stock[] = $product;
+            }
         }
+
+        $attribute = $this->attribute;
         $brands = $this->brands;
-        $products = $this->arrayPaginator(array_merge($in_stock,$not_stock), $request,$this->pre_products );
+        $products = $this->arrayPaginator($this->filterProduct(), $request,$this->pre_products );
         $products->withPath($request->fullUrl());
         return view('catalog.index',compact('products','brands','min_price','max_price','attribute'));
     }
@@ -146,5 +149,94 @@ class CatalogController extends Controller
             session()->forget('pre_products');
             session(['pre_products' => (int)$request->pre_show]);
         }
+        if (isset($request->min_price) && isset($request->max_price)){
+            session()->forget('filter.min_price');
+            session()->forget('filter.max_price');
+            session(['filter' => [
+                'min_price' => (float)str_replace('₴','',$request->min_price),
+                'max_price' => (float)str_replace('₴','',$request->max_price)
+            ]]);
+        }
+        if (isset($request->clear_filter)){
+            session()->forget('filter');
+        }
+        if (isset($request->supplierid) && isset($request->active)){
+            if ($request->active === 'true'){
+                session()->put("filter.suppliers.{$request->supplierid}",$request->supplierid);
+            } else {
+                session()->forget("filter.suppliers.{$request->supplierid}");
+                if (empty(session('filter.suppliers'))){
+                    session()->forget('filter.suppliers');
+                }
+            }
+        }
+        if(isset($request->attrFilter) && isset($request->active)){
+            $data = explode('@',$request->attrFilter,2);
+            if ($request->active === 'true'){
+                if (session()->has("filter.attributes.{$data[0]}")){
+                    $buff = session("filter.attributes.{$data[0]}");
+                    array_push($buff,$data[1]);
+                    session()->put("filter.attributes.{$data[0]}",$buff);
+                }else{
+                    session()->put("filter.attributes.{$data[0]}",[$data[1]]);
+                }
+            } else {
+                $buff = session("filter.attributes.{$data[0]}");
+                foreach ($buff as $k => $item){
+                    if($item === $data[1]){
+                        unset($buff[$k]);
+                    }
+                }
+                if (empty($buff)){
+                    session()->forget("filter.attributes.{$data[0]}");
+                }else{
+                    session()->put("filter.attributes.{$data[0]}",$buff);
+                }
+                if (empty(session('filter.attributes'))){
+                    session()->forget('filter.attributes');
+                }
+            }
+        }
+    }
+
+    protected function filterProduct(){
+        if(!session()->has('filter.min_price') && !session()->has('filter.max_price')){
+            $products = array_merge($this->in_stock,$this->not_stock);
+        } else {
+            $products = $this->in_stock;
+        }
+
+        foreach ($products as $k => $product){
+            if (session()->has('filter.min_price') && session()->has('filter.max_price')){
+                if ($product->price < session('filter.min_price') && $product->price > session('filter.max_price')){
+                    unset($products[$k]);
+                }
+            }
+            if (session()->has('filter.suppliers') && !empty(session('filter.suppliers'))){
+                foreach (session('filter.suppliers') as $supplier){
+                    if ((int)$supplier === $product->supplierId){
+                        break;
+                    }
+                    unset($products[$k]);
+                }
+            }
+            if (session()->has('filter.attributes') && !empty(session('filter.attributes'))){
+                $delete = true;
+                foreach (session('filter.attributes') as $kay => $attributes){
+                    foreach ($attributes as $attribute){
+                        foreach ($product->filter_data as $attr_prod){
+                            if ($attr_prod->id === $kay && $attribute === $attr_prod->displayvalue){
+                                $delete = false;
+                            }
+                        }
+                    }
+                }
+                if ($delete){
+                    unset($products[$k]);
+                }
+            }
+        }
+
+        return $products;
     }
 }
--- resources/views/catalog/compenents/filter.blade.php
+++ resources/views/catalog/compenents/filter.blade.php
@@ -1,11 +1,17 @@
 <div class="shop-side-bar" style="max-height: 1010px;overflow: auto;">
-
+    @if(session()->has('filter'))
+        <a onclick="clearFilter(this)" class="btn btn-default close-filter" href="#" role="button" title="{{__('очистить фильтр')}}">
+            <i class="fa fa-times" aria-hidden="true"></i>
+        </a>
+    @endif
     @if((int)$min_price > 0)
     <h6>Price</h6>
     <!-- PRICE -->
     <div class="cost-price-content">
         <div id="price-range" class="price-range"></div>
-        <span id="price-min" class="price-min">20</span> <span id="price-max" class="price-max">80</span> <a href="#." class="btn-round" >{{__('Фильтровать')}}</a>
+        <span id="price-min" class="price-min">20</span>
+        <span id="price-max" class="price-max">80</span>
+        <a href="#." onclick="setPrice()" class="btn-round" >{{__('Фильтровать')}}</a>
     </div>
     @endif
     <!-- Featured Brands -->
@@ -14,8 +20,9 @@
         <ul>
             @isset($brands)
                 @foreach($brands as $brand)
+                    @php $filter_brand = session("filter.suppliers.{$brand->supplierId}"); @endphp
                     <li>
-                        <input id="brand{{$brand->supplierId}}" class="styled" type="checkbox" >
+                        <input @isset($filter_brand) checked @endisset onchange="setSupplier(this)" id="brand{{$brand->supplierId}}" value="{{$brand->supplierId}}" class="styled" type="checkbox" >
                         <label for="brand{{$brand->supplierId}}">
                             {{$brand->description}}
                         </label>
@@ -36,9 +43,20 @@
                         <ul>
                 @endif
                 @if(!in_array($item->displayvalue,$buff))
+                     @php
+                         $filter_attr_data = session("filter.attributes.{$item->id}");
+                         $filter_attr = null;
+                         if (isset($filter_attr_data)){
+                            foreach ($filter_attr_data as $val){
+                                if ($val === $item->displayvalue){
+                                    $filter_attr = true;
+                                }
+                            }
+                         }
+                     @endphp
                             <li>
-                                <input value="{{$item->id}}" id="cate_{{$item->id .'_'. $k}}" class="styled" type="checkbox" >
-                                <label for="cate1">{{$item->displayvalue}}</label>
+                                <input @isset($filter_attr) checked @endisset onchange="setAttrFilter(this)" value="{{__("{$item->id}@{$item->displayvalue}")}}" id="cate_{{$item->id .'_'. $k}}" class="styled" type="checkbox" >
+                                <label for="cate_{{$item->id .'_'. $k}}">{{$item->displayvalue}}</label>
                             </li>
                     @php array_push($buff,$item->displayvalue); @endphp
                 @endif
@@ -57,10 +75,10 @@
         //  Price Filter ( noUiSlider Plugin)
         $("#price-range").noUiSlider({
             range: {
-                'min': [ {{(int)$min_price}} ],
-                'max': [ {{(int)$max_price}} ]
+                'min': [ {{(float)$min_price}} ],
+                'max': [ {{(float)$max_price}} ]
             },
-            start: [ {{(int)$min_price}} , {{(int)$max_price}}],
+            start: [ {{session()->has('filter.min_price')?session('filter.min_price'):(float)$min_price}} , {{session()->has('filter.max_price')?session('filter.max_price'):(float)$max_price}}],
             connect:true,
             serialization:{
                 lower: [
@@ -80,6 +98,25 @@
                 }
             }
         })
-    })
+    });
+
+    function setPrice() {
+        $.get(`{{route('filter')}}?min_price=${$('#price-min').text()}&max_price=${$('#price-max').text()}`,()=>{location.reload()});
+    }
+
+    function clearFilter(obj) {
+        $(obj).html('<i class="fa-li fa fa-spinner fa-spin">');
+        $.get(`{{route('filter')}}?clear_filter=true`,()=>{location.reload()});
+    }
+
+    function setSupplier(obj) {
+        const data = $(obj);
+        $.get(`{{route('filter')}}?supplierid=${data[0].value}&active=${data[0].checked}`,()=>{location.reload()});
+    }
+
+    function setAttrFilter(obj) {
+        const data = $(obj);
+        $.get(`{{route('filter')}}?attrFilter=${data[0].value}&active=${data[0].checked}`,()=>{location.reload()});
+    }
 
 </script>
