c2a71cc54a6671ee098a61d71cdae803fe0fc3de
Seliv <selivonenko007@gmail.com>
2019-05-20 17:49:05 +0300

    update search for article 2

--- resources/views/catalog/partirals/list_product.blade.php
+++ resources/views/catalog/partirals/list_product.blade.php
@@ -35,7 +35,7 @@
                                 <span onclick="productInfo('{{$data->articles}}' @isset($data->supplierId) ,'{{$data->supplierId}}' @endisset )" class="product-info-icon" data-toggle="modal" data-target="#productInfoModal" title="Больше инфи">
                                     <i class="fa fa-info-circle" aria-hidden="true"></i>
                                 </span>
-                                <a class="alternate-link" href="{{route('alternate',['article' => $data->articles,'supplierId' =>isset($data->supplierId)?$data->supplierId:''])}}">аналоги</a>
+                                @isset($data->supplierId)<a class="alternate-link" href="{{route('alternate',['article' => $data->articles,'supplierId' => $data->supplierId])}}">аналоги</a>@endisset
                             </div>
                         </div>
                         <div class="list-product-wrapper">
