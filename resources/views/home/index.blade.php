6d059fbbb258ccf335185c1b52c3941bfe3f3b44
Seliv <selivonenko007@gmail.com>
2019-06-24 16:54:49 +0300

    change liqpay key,add link in home

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -45,6 +45,32 @@
             @endcomponent
         @endif
 
+        <div class="container">
+            <div class="row padding-bottom-15">
+                <div class="col-sm-12">
+                    <div class="seo_glupost text-center">
+                        <a href="#">Автозапчасти</a><span> |</span>
+                        <a href="#">Авто запчасти</a><span> |</span>
+                        <a href="#">Запчасти</a><span> |</span>
+                        <a href="#">Автозапчасти Киев</a><span> |</span>
+                        <a href="#">Запчасти Киев</a><span> |</span>
+                        <a href="#">Запчасти для иномарок</a><span> |</span>
+                        <a href="#">Купить запчасти</a><span> |</span>
+                        <a href="#">Интернет магазин автозапчастей</a><span> |</span>
+                        <a href="#">Автозапчасти для иномарок</a><span> |</span>
+                        <a href="#">Интернет магазин запчастей</a><span> |</span>
+                        <a href="#">Купить автозапчасти</a><span> |</span>
+                        <a href="#">Автозапчасти Украина</a><span> |</span>
+                        <a href="#">Продажа автозапчастей</a><span> |</span>
+                        <a href="#">Магазин автозапчастей</a><span> |</span>
+                        <a href="#">Каталог запчастей</a><span> |</span>
+                        <a href="#">Поиск запчастей</a><span> |</span>
+                        <a href="#">Кузовные запчасти</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+
     </div>
     <!-- End Content -->
 
