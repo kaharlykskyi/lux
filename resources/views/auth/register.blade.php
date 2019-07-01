b0ee9fb0b26d735e4c83142ad199d0d6e62ea9bb
Vasylevs <selivonenko007@gmail.com>
2019-07-01 17:35:31 +0300

    register alert info

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -17,7 +17,12 @@
                 <div class="row">
                     <div class="col-xs-12 col-sm-offset-2 col-sm-8">
                         <h5 class="text-center margin-bottom-20">{{ __('Регистрация') }}</h5>
-
+                        <div class="alert alert-warning" role="alert">
+                            Все поля обезательны к заполнению
+                            <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                                <span aria-hidden="true">&times;</span>
+                            </button>
+                        </div>
                         <form method="POST" action="{{ route('register') }}">
                             @csrf
 
